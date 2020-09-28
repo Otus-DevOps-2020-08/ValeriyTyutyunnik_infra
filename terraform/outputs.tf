4eaa973372893eb62fbdf8d52f2370663f551cb6
ValeriyTyutyunnik <valeriytyutyunnik@yandex.ru>
2020-09-28 22:08:04 +0300

    Db and App, split into more files configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,6 @@ output "external_ip_address_app" {
   description = "IP address of the reddit-app instances."
   value       = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
 }
+output "external_ip_address_db" {
+  value = yandex_compute_instance.db.network_interface.0.nat_ip_address
+}
