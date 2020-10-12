e226bd6b369805c08c9c497b26a22ba9af07d757
ValeriyTyutyunnik <valeriytyutyunnik@yandex.ru>
2020-10-12 23:25:16 +0300

    ansible for packer images

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -12,10 +12,6 @@ variable zone {
   default = "ru-central1-a"
 }
 
-#variable image_id {
-#  description = "Disk image"
-#}
-
 variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
