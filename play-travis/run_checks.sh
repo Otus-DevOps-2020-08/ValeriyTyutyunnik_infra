41b054d0ff68355a6d2fa5e16191928d43d34a7b
ValeriyTyutyunnik <valeriytyutyunnik@yandex.ru>
2020-10-15 03:09:59 +0300

    fix checks

--- play-travis/run_checks.sh
+++ play-travis/run_checks.sh
@@ -5,10 +5,7 @@ pwd
 
 #if [ ( $TRAVIS_BRANCH == "master" && $TRAVIS_EVENT_TYPE == "push" ) || $TRAVIS_EVENT_TYPE == "pull_request" ]; then
   for f in play-travis/*_check.sh; do
-
-    docker exec hw-test bash -c 'sh pwd'
-    docker exec hw-test bash -c 'sh tree'
-    docker exec hw-test bash -c 'sh play-travis/$f'
+    $f
   done
 #else
 #  echo 'skipping checks';
