70cb79864237a8e1a973ee027b90d383ef1067c0
ValeriyTyutyunnik <valeriytyutyunnik@yandex.ru>
2020-10-15 04:23:52 +0300

    fix syntax

--- play-travis/run_checks.sh
+++ play-travis/run_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ( $TRAVIS_BRANCH == "master" && $TRAVIS_EVENT_TYPE == "push" ) || $TRAVIS_EVENT_TYPE == "pull_request" ]; then
+if [[ ( $TRAVIS_BRANCH == "master" && $TRAVIS_EVENT_TYPE == "push" ) || $TRAVIS_EVENT_TYPE == "pull_request" ]]; then
   docker exec hw-test bash -c './play-travis/ansible_check.sh'
   docker exec hw-test bash -c './play-travis/packer_check.sh'
   docker exec hw-test bash -c './play-travis/terraform_check.sh'
