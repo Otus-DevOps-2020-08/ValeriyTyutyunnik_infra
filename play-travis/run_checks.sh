49e1c3b34f07648a510933f187cc11bae4c37a9c
ValeriyTyutyunnik <valeriytyutyunnik@yandex.ru>
2020-10-17 18:02:03 +0300

    fix travis test

--- play-travis/run_checks.sh
+++ play-travis/run_checks.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+DOCKER_IMAGE=express42/otus-homeworks:0.7.1
 
 if [[ ( $TRAVIS_BRANCH == "master" && $TRAVIS_EVENT_TYPE == "push" ) || $TRAVIS_EVENT_TYPE == "pull_request" ]]; then
+
+  docker network create hw-test-net
+  docker run -d -v $(pwd):/srv -v /var/run/docker.sock:/tmp/docker.sock \
+         -e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN -p 33433:22 --privileged \
+         --device /dev/net/tun --name hw-test --network hw-test-net $DOCKER_IMAGE
+
   docker exec hw-test bash -c './play-travis/ansible_check.sh'
   docker exec hw-test bash -c './play-travis/packer_check.sh'
   docker exec hw-test bash -c './play-travis/terraform_check.sh'
